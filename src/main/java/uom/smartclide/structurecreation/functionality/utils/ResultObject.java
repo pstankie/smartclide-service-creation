307384fee139bcbd5f0e7ad4a50bb40d7ac7d80f
Theodoros Maikantis <teomaik19@gmail.com>
2022-01-06 19:05:29 +0200

    added copyright to ResultObject

--- src/main/java/uom/smartclide/structurecreation/functionality/utils/ResultObject.java
+++ src/main/java/uom/smartclide/structurecreation/functionality/utils/ResultObject.java
@@ -1,5 +1,15 @@
 package uom.smartclide.structurecreation.functionality.utils;
 
+/*
+ * Copyright (C) 2021 UoM - University of Macedonia
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
 public class ResultObject {
 	int status;
 	String message;

3f0b129e396bdc3ab9b1abbb5714a87ec8e05e95
Theodoros Maikantis <teomaik19@gmail.com>
2022-01-06 19:04:39 +0200

    added Copyright to PipelineConfig

--- src/main/java/uom/smartclide/structurecreation/functionality/utils/PipelineConfigXML.java
+++ src/main/java/uom/smartclide/structurecreation/functionality/utils/PipelineConfigXML.java
@@ -1,5 +1,15 @@
 package uom.smartclide.structurecreation.functionality.utils;
 
+/*
+ * Copyright (C) 2021 UoM - University of Macedonia
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;

1a58a025c9a121de4c01f2144d61996fc9d99f77
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-27 22:50:11 +0200

    Adding copyrights and reformate existing headers
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- src/main/java/uom/smartclide/structurecreation/ServiceCreationApplication.java
+++ src/main/java/uom/smartclide/structurecreation/ServiceCreationApplication.java
@@ -1,14 +1,13 @@
-package uom.smartclide.structurecreation;
-
-/*
- * Copyright (C) 2021 UoM - University of Macedonia
+/*******************************************************************************
+ * Copyright (C) 2021-2022 University of Macedonia
  * 
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License 2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0/
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  * 
  * SPDX-License-Identifier: EPL-2.0
- */
+ ******************************************************************************/
+package uom.smartclide.structurecreation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
